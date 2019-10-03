37d27519c874692b4cb78bda8c4dcf4fb88cf259
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-10-02 20:22:50 -0400

    Example of an implementation detail

--- Sources/KevinCCode/include/kevin.h
+++ Sources/KevinCCode/include/kevin.h
@@ -2,7 +2,24 @@
 #define IN_KEVIN
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void printHello();
+double getDataFromImplementationDetail();
+double * implementationDetail();
+
+
+double * implementationDetail() {
+
+    double * allocated = malloc(sizeof(double) * 10);
+
+    int i;
+
+    for(i=0; i<10; i++) {
+        allocated[i] = 0.5;
+    }
+
+    return allocated;
+}
 
 #endif
--- Sources/KevinCCode/kevin.c
+++ Sources/KevinCCode/kevin.c
@@ -1,5 +1,13 @@
 #include "include/kevin.h"
 
+
 void printHello() {
     printf("Hello from C and the SNERKLOR subsystem\n");
+    printf("Data from Implementation:  %f\n", getDataFromImplementationDetail());
 }
+
+double getDataFromImplementationDetail() {
+    double * detail = implementationDetail();
+    return detail[1];
+}
+
